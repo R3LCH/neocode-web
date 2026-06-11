9b051055dc2b4db601d2f914e2a7c1a4db0cac5a
R3LCH <imr3lch@gmail.com>
2026-06-11 23:28:22 +0300

    Set up GitHub Pages deploy, vendor protocol schema

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@protocol": path.resolve(__dirname, "../protocol"),
+      "@protocol": path.resolve(__dirname, "protocol"),
     },
   },
   server: {
